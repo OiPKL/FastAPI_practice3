06b45fc9e137307ac3374d2ce3e0ade3c16d452a
dhkswls1998 <dhkswls1998@naver.com>
2023-11-16 19:47:07 +0900

    connect complete

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.database.sqlite import engine
 from app.routers import router_user
@@ -10,11 +10,10 @@ from app.routers import router_garden
 app = FastAPI()
 
 # CORS 설정 >> 주석처리 변경
-# origins = ["http://192.168.74.31:8080"]
+origins = ["null"]
 # origins = ["http://localhost:8080"]
-# origins = ["https://localhost:8080"]
-origins = ["http://59.5.235.142:8080"]
-# origins = ["https://59.5.235.142:8080"]
+# origins = ["http://192.168.74.31:8080"]
+# origins = ["http://59.5.235.142:8080"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,6 +22,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.middleware("http")
+async def add_cors_header(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "null"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
 
 from app.database.model import Base as UserBase
 from app.database.model import Base as VegetableBase
@@ -39,20 +45,3 @@ app.include_router(router_garden.router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
-
-# if __name__ == '__main__':
-#     import uvicorn
-#     from subprocess import Popen
-
-#     Popen(['python', '-m', 'https_redirect'])
-#     uvicorn.run(
-#         'main:app', host="0.0.0.0", port=8080,
-#         reload=True, reload_dirs=['html_files'],
-#         ssl_keyfile='C:/Users/dhksw/Documents/work/key.pem',
-#         ssl_certfile='C:/Users/dhksw/Documents/work/cert.pem'
-#     )
-    
-
-
-# uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload
-# uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload --ssl-keyfile=./key.pem --ssl-certfile=./cert.pem
